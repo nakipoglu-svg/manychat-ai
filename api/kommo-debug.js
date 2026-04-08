62ccf0234d68955969f2e115bf793f013ed1ed36
nakipoglu-svg <nakipoglu@gmail.com>
2026-04-08 19:07:08 +0300

    Delete api/kommo-debug.js

--- api/kommo-debug.js
+++
@@ -1,6 +0,0 @@
-export default async function handler(req, res) {
-  console.log("KOMMO HIT");
-  console.log("method:", req.method);
-  console.log("FULL BODY:", JSON.stringify(req.body || {}).slice(0, 3000));
-  return res.status(200).json({ ok: true });
-}
